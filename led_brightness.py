481af7412effc8650db657b331c0edef61e3ab05
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-10-13 16:16:01 +0000

    format

--- led_brightness.py
+++ led_brightness.py
@@ -2,7 +2,6 @@
 import cgi
 import json
 
-print("Content-type: text/html\n\n")
 data = cgi.FieldStorage()
 s1 = data.getvalue('LED')
 s2 = data.getvalue('slider')
@@ -20,13 +19,12 @@ Content-type:text/html\n\n
 <div style="width:600px;background:#71F282;border:1px;text-align:center">
 <br>
 <font size="3" color = "black" face = "helvetica">
-<b>LED Brightness</b>
 <br>
 """)
 
-if s1 ==13:
+if s1 == str(13):
   print('RED LED BRIGHTNESS = %s' %s2) 
-elif s1 ==19:
+elif s1 ==str(19):
   print('WHITE LED BRIGHTNESS = %s' %s2) 
 else:
   print('BLUE LED BRIGHTNESS = %s' %s2) 
@@ -34,10 +32,10 @@ else:
 
 print("""
 <form action = "/cgi-bin/led_brightness.py" method = "POST">
-  <input type = "radio" name = "LED" value = "13" checked> LED 1<br>
+  <input type = "radio" name = "LED" value = "13"> LED 1<br>
   <input type = "radio" name = "LED" value = "19"> LED 2<br>
   <input type = "radio" name = "LED" value = "26"> LED 3<br>
-  <input type ="range" name = "slider" min = "0" max="100" value="0"><br>
+  <input type ="range" name = "slider" min = "0" max="100" value="%s"><br> %s2
   <input type="submit" value = "Change LED brightness">
 </form>
 </div>
