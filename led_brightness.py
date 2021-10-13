1455078c48548d8fa6ae6c406d151ba65b8f098a
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-10-13 15:18:33 +0000

    print

--- led_brightness.py
+++ led_brightness.py
@@ -2,13 +2,13 @@
 import cgi
 import json
 
+print("Content-type: text/html\n\n")
 data = cgi.FieldStorage()
 s1 = data.getvalue('menu_choice')
-print("Content-type: text/html\n\n")
-print("selection = " + s1)
+print("selection = " + data.getvalue('menu_choice'))
 s2 = data.getvalue('slider')
 print("Content-type: text/html\n\n")
-print("Duty Cycle = " + str(s2))
+print("Duty Cycle = " + data.getvalue('slider'))
 data = {"menu_choice":s1, "slider":s2}
 with open('led_brightness_multiple.txt','w') as f:
   json.dump(data,f)
